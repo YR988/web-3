670eed5d4a7d899d25fa42d679e81427e1424cf7
YR988 <arseniysavinov@gmail.com>
2024-12-14 14:42:29 +0300

    update

--- projects/digits/main.go
+++ projects/digits/main.go
@@ -1,7 +1,13 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Hello, world!!!")
+package main 
+import "fmt" 
+func main() { 
+var myString string 
+fmt.Scan(&myString) 
+var mx rune 
+for _, v := range myString { 
+if v > mx { 
+mx = v 
+} 
+} 
+fmt.Printf("%c", mx) 
 }
